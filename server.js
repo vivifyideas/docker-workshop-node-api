7241c61c584e7c8b4f269b43e0c89e2c2408cfbf
Aleksandar Babic <aleksandar.b@vivifyideas.com>
2019-04-16 14:34:35 +0200

    Refactor

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-var express = require('express');
-// var path = require('path');
-// var mysql = require('mysql');
-var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser')
 
 app.set('port', 3000);
 app.use(bodyParser.json())
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.send({
     file: fs.readFileSync('./data/message.txt', 'utf8')
   });
 });
 
-app.post('/', function(req, res){
+app.post('/', (req, res) => {
   const text = req.body.text;
 
   fs.writeFile('./data/message.txt', text, 'utf8', (err) => {
